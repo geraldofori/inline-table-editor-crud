466df4d54e74b6bd5c894175d83f974c14743e3b
geraldofori <oforige@gmail.com>
2023-06-09 14:44:47 +0000

    changes to the view for the listing all the users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./data/users/Users";
+import {UsersData} from "./data/users/UsersData";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'inline-table-editor-crud';
+export class AppComponent implements OnInit{
+
+  users: User[] = [];
+
+  constructor() {
+  }
+  ngOnInit() : void{
+    this.users = UsersData;
+  }
+
+
 }
---
+++ src/app/data/users/UsersData.ts
@@ -0,0 +1,61 @@
+import {User} from "./Users";
+
+export const UsersData: User[] = [
+  {
+    "id": 0,
+    "name": "Cleo Smith",
+    "email": "cleosmith@earwax.com",
+    "description": "Et pariatur fugiat non non aute exercitation proident ad.",
+    "company": "Cubix"
+  },
+  {
+    "id": 1,
+    "name": "Henderson Daugherty",
+    "email": "hendersondaugherty@cubix.com",
+    "description": "Dolor in esse labore minim exercitation cillum velit in in Lorem elit velit occaecat.",
+    "company": "Insuresys"
+  },
+  {
+    "id": 2,
+    "name": "Lancaster Carey",
+    "email": "lancastercarey@insuresys.com",
+    "description": "Mollit qui laboris veniam sunt consequat in.",
+    "company": "Tsunamia"
+  },
+  {
+    "id": 3,
+    "name": "Marsh Best",
+    "email": "marshbest@tsunamia.com",
+    "description": "Eiusmod reprehenderit excepteur anim irure ullamco ad.",
+    "company": "Geekwagon"
+  },
+  {
+    "id": 4,
+    "name": "Moreno Buchanan",
+    "email": "morenobuchanan@geekwagon.com",
+    "description": "Laboris enim veniam officia dolore enim aute nulla.",
+    "company": "Geekosis"
+  },
+  {
+    "id": 5,
+    "name": "Becky Sullivan",
+    "email": "beckysullivan@geekosis.com",
+    "description": "Do ea ex sint aliqua ad consequat velit.",
+    "company": "Comdom"
+  },
+  {
+    "id": 6,
+    "name": "Greene Odonnell",
+    "email": "greeneodonnell@comdom.com",
+    "description": "Laborum deserunt qui exercitation nulla duis veniam est commodo pariatur fugiat velit minim ad.",
+    "company": "Deepends"
+  },
+  {
+    "id": 7,
+    "name": "Hays Wade",
+    "email": "hayswade@deepends.com",
+    "description": "Aute reprehenderit eiusmod magna esse sit tempor labore ea duis sit amet velit tempor ex.",
+    "company": "Dognost"
+  }
+]
+
